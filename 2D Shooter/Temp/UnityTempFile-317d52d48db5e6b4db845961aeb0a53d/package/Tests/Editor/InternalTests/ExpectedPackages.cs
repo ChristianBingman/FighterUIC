2c1dd27e16fc26e4d3a5f7885ae1d41ffa52c7c7
Evangelos Sfyris <61063287+EvangelosSfyris@users.noreply.github.com>
2020-05-13 18:34:44 -0500

    Basic movement testin
    general purpose movement script and ability to test it

---
+++ 2D Shooter/Temp/UnityTempFile-317d52d48db5e6b4db845961aeb0a53d/package/Tests/Editor/InternalTests/ExpectedPackages.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using PackageManager = UnityEditor.PackageManager;
+
+namespace Tests
+{
+    public class ExpectedPackages
+    {
+        private static string[] expectedPackages =
+        {
+            "com.unity.2d.sprite",
+            "com.unity.2d.tilemap",
+        };
+
+        [Test]
+        public void HasExpectedPackages()
+        {
+            var listRequest = PackageManager.Client.List(true);
+            while (!listRequest.IsCompleted)
+                System.Threading.Thread.Sleep(10);
+            if (listRequest.Status == PackageManager.StatusCode.Failure)
+                Assert.Fail("Failed to list local packages");
+
+            var existingPackages = new List<PackageManager.PackageInfo>(listRequest.Result);
+            foreach (var expectedPackage in expectedPackages)
+            {
+                if (!existingPackages.Exists(packageInfo => packageInfo.name.Equals(expectedPackage)))
+                    Assert.Fail("{0} is not in the project template.", expectedPackage);
+            }
+        }
+    }
+}
