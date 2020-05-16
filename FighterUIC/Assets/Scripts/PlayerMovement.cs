1bdb72982474ad6fa08296e4042538c76adbe657
Evangelos Sfyris <61063287+EvangelosSfyris@users.noreply.github.com>
2020-05-15 22:08:55 -0500

    Script changes
    stuff needs fixing, but partial progress

--- FighterUIC/Assets/Scripts/PlayerMovement.cs
+++ FighterUIC/Assets/Scripts/PlayerMovement.cs
@@ -2,46 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/* BASIC MOVEMENT SCRIPT ORIGINALLY FROM UNITY DOCUMENTATION */
 
 public class PlayerMovement : MonoBehaviour
 {
-    CharacterController characterController;
 
     public float moveSpeed = 6.0f;
-    public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
-
+    private Rigidbody rigidbody;
     private Vector3 moveDirection = Vector3.zero;
 
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        rigidbody = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        if (characterController.isGrounded)
-        {
-            // We are grounded, so recalculate
-            // move direction directly from axes
+        // Get horizontal input
+        float horizontal = Input.GetAxis("Horizontal");
 
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-            moveDirection *= moveSpeed;
+        // Get vertical input
+        float vertical = Input.GetAxis("Vertical");
 
-            if (Input.GetButton("Jump"))
-            {
-                moveDirection.y = jumpSpeed;
-            }
-        }
+        // Create a new vector using horizontal and vertical movement
+        moveDirection = new Vector3(horizontal, vertical);
 
-        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-        // as an acceleration (ms^-2)
-        moveDirection.y -= gravity * Time.deltaTime;
+        // Apply movement speed to the rigidbody
+        rigidbody.AddForce(moveDirection * moveSpeed);
 
-        // Move the controller
-        characterController.Move(moveDirection * Time.deltaTime);
     }
 
 }
