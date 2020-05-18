6cf6fb92c9fd3ecc9053d8c5772eac61f9bafd28
ChristianBingman <christianbingman@gmail.com>
2020-05-18 00:03:14 -0500

    Added a script that allows the camera to keep a single object in view
    Added a basic script that allows the camera to keep a single object in view along with adding the W button as a Jump Button.

---
+++ FighterUIC/Assets/Scripts/KeepInView.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepInView : MonoBehaviour
+{
+    // Object to keep in view (Add more in future)
+    public GameObject objectInView;
+
+    Transform CameraPos;
+    Camera    CameraObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get current camera location
+        CameraPos = GetComponent<Transform>();
+        CameraObj = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the location of the player relative to the Viewport
+        Vector3 pointOnScreen = CameraObj.WorldToViewportPoint(objectInView.transform.position);
+        // Determine if the object is on the screen
+        bool isInBoundsX = pointOnScreen.x > 0.1 && pointOnScreen.x < 0.9;
+        bool isInBoundsY = pointOnScreen.y > 0.1 && pointOnScreen.y < 0.9;
+
+        // If object is not on the screen zoom out
+        if (!isInBoundsX)
+        {
+            CameraPos.position += Vector3.back * 0.1f;
+        }
+
+    }
+}
